9b873294ce943c15ca91aee4cd65aa818038827b
justin.lin <justin.lin@yxunistar.com>
2021-09-06 15:23:42 +0800

    style: add reset style and global style

---
+++ src/theme/globalStyles.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    width: 100vw;
+    height: 100vh;
+  }
+`;
+
+export default GlobalStyle;
