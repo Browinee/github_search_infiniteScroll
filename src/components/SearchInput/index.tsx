7cfcb84c40c69d41c2c55aaa0d75b4e09bf3bce9
justin.lin <justin.lin@yxunistar.com>
2021-09-06 15:30:31 +0800

    feat:  add searchInput

---
+++ src/components/SearchInput/index.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import {noop} from "../../utils";
+
+
+type SearchInputType = {
+    type?: string;
+    placeholder?: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+
+}
+
+const Input = styled.input`
+  padding: 10px 20px;
+  border: solid 2px #686868;
+  border-radius: 5px;
+  margin-right: 5px;
+  outline: none;
+
+  &:focus {
+    border-color: #005ad8;
+    transition: 0.5s;
+  }
+`
+const SearchInput = (props: SearchInputType) => {
+    const {type = "text", placeholder = "Please enter the repository", value = "", onChange = noop} = props;
+
+    return <Input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+    />
+}
+
+
+export default SearchInput;
