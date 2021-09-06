25ff625d8ef9fa51fac0f758fdc838c142f60cd6
justin.lin <justin.lin@yxunistar.com>
2021-09-06 19:08:55 +0800

    feat: add Card

---
+++ src/components/Card/StyledCompnents.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const Container = styled.article`
+  width: 100%;
+  border-top: 1px solid rgb(233, 233, 233);
+  border-bottom: 1px solid rgb(233, 233, 233);
+  padding: 10px;
+  & + & {
+    border-top: 0;
+  }
+  
+`
+
+
+export const Title = styled.header`
+  font-size: 2rem;
+  width: 100%;
+  color: black;
+  margin-bottom: 10px;
+`
+
+export const Description = styled.p`
+    color: rgba(0, 0, 0, 0.75);
+  margin-bottom: 10px
+`;
+export const Footer = styled.footer`
+  display: flex;
+`
+
+export const Star = styled.div``;
+export const Lang = styled.div``;
+export const License = styled.div``;
+export const UpdateTime = styled.div``;
---
+++ src/components/Card/index.tsx
@@ -0,0 +1,35 @@
+import {Container, Title, Footer, Description, Star, Lang, UpdateTime, License} from "./StyledCompnents";
+import {AiFillStar, AiTwotoneCheckCircle} from "react-icons/ai";
+import {getLastUpdated, getLicense} from "../../utils";
+
+type CardProps = {
+    full_name: string;
+    description: string;
+    stargazers_count: number;
+    license: any; // refactor
+    updated_at: string;
+    language: string;
+}
+
+const Card = (props: CardProps) => {
+    console.log("props", props);
+    const {full_name = "", description = "", stargazers_count = 0, license = "", updated_at = "", language = ""} = props;
+    return <Container>
+        <Title>
+            {full_name}
+        </Title>
+        <Description>
+            {description}
+        </Description>
+        <Footer>
+            <Star><AiFillStar />{stargazers_count}</Star>
+            <Lang><AiTwotoneCheckCircle />{language}</Lang>
+            <License>{getLicense(license)}</License>
+            <UpdateTime>Updated at {getLastUpdated(updated_at)}</UpdateTime>
+
+        </Footer>
+    </Container>
+};
+
+
+export default Card;
