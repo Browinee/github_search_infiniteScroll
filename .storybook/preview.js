18f32d9dd27564acbf1f77935be7dad83bd42bb1
justin.lin <justin.lin@yxunistar.com>
2021-09-08 22:43:56 +0800

    build: add husky, prettier, commitlint

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,15 @@
-import {ThemeProvider} from "styled-components";
+import { ThemeProvider } from "styled-components";
 import THEME from "../src/theme/theme";
-
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-  },
-}
+  controls: {},
+};
 
 export const decorators = [
   (Story) => (
-      <ThemeProvider theme={THEME}>
-          <Story/>
-      </ThemeProvider>
+    <ThemeProvider theme={THEME}>
+      <Story />
+    </ThemeProvider>
   ),
-]
+];
--- README.md
+++ README.md
@@ -1,23 +1,20 @@
 ## Todo
+
 - [ ] error handleing - error boundarly - 502 404
-- [ ] storybook
+- [x] storybook
 - [ ] test
 - [ ] lighthouse
 - [ ] reportWebVitals - 內容確認
 - [ ]router
-- []  format 
-
-
+- [] lint husky
 
 feature:
+
 - handle rate limit
 - handle error status
 - //Status: 503 Service Unavailable
 - // Status: 422 Unprocessable Entity
 
-
 storybook
-
-
 
 testing
