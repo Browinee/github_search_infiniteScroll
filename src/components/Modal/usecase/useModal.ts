d1736b536f5db359a6069ef080c25baa301fcadb
justin.lin <justin.lin@yxunistar.com>
2021-09-08 19:48:57 +0800

    feat: add modal and hookk

---
+++ src/components/Modal/usecase/useModal.ts
@@ -0,0 +1,20 @@
+import {useCallback, useEffect, useState} from "react";
+
+type useModalProps = {
+    isModalOpen: boolean;
+    toggleModal: (status: boolean) => void;
+}
+const useModal = (initialStatus?: boolean): useModalProps => {
+    const [isModalOpen, setIsModalOpen] = useState(initialStatus || false);
+    const toggleModal = useCallback((status) => {
+        setIsModalOpen(status);
+    }, []);
+    useEffect(() => {
+        setIsModalOpen(initialStatus || false);
+    }, [initialStatus])
+    return {isModalOpen, toggleModal};
+
+}
+
+
+export default useModal;
